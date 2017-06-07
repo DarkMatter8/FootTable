a9e7098bc4e2d2fe4dbcb4bed0c1e1fee11a9ecc
Darkmatter865 <darkmatter865@gmail.com>
2017-06-08 00:52:47 +0530

    Assets remaining

--- app/src/main/java/com/example/vinay/tabactivity/Fragment5.java
+++ app/src/main/java/com/example/vinay/tabactivity/Fragment5.java
@@ -1,5 +1,8 @@
 package com.example.vinay.tabactivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -30,73 +33,82 @@ public class Fragment5 extends Fragment {
 
     private AdapterTeam mAdapter;
     public String jsondata;
+    public String matchweek;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.tab5,container,false);
 
 
+        ConnectivityManager connectivityManager = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
 
-        String jsonURL = "http://api.football-data.org/v1/competitions/434/leagueTable";
+            final View view = inflater.inflate(R.layout.tab5, container, false);
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(jsonURL)
-                .header("X-Auth-Token","a75aa1e62d6c4c87bd9fc71f7de21358")
-                .build();
+            String jsonURL = "http://api.football-data.org/v1/competitions/434/leagueTable";
 
-        okhttp3.Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-                Toast.makeText(getActivity(),"some error",Toast.LENGTH_SHORT).show();
-            }
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(jsonURL)
+                    .header("X-Auth-Token", "a75aa1e62d6c4c87bd9fc71f7de21358")
+                    .build();
 
-            @Override
-            public void onResponse(okhttp3.Call call, Response response) throws IOException {
-                jsondata = response.body().string();
+            okhttp3.Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(okhttp3.Call call, IOException e) {
+                    Toast.makeText(getActivity(), "Some error", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                    jsondata = response.body().string();
 
 
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            List<Team> data=new ArrayList<>();
-                            JSONObject json = new JSONObject(jsondata);
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                List<Team> data = new ArrayList<>();
+                                JSONObject json = new JSONObject(jsondata);
+                                matchweek = json.getString("matchday");
+                                JSONArray jArray = json.getJSONArray("standing");
 
-                            JSONArray jArray = json.getJSONArray("standing");
+                                for (int i = 0; i < jArray.length(); i++) {
+                                    JSONObject json_data = jArray.getJSONObject(i);
+                                    Team TData = new Team();
+                                    TData.name = json_data.getString("teamName");
+                                    TData.points = json_data.getString("points");
+                                    TData.played = json_data.getString("playedGames");
+                                    TData.win = json_data.getString("wins");
+                                    TData.draw = json_data.getString("draws");
+                                    TData.lose = json_data.getString("losses");
+                                    TData.gd = json_data.getString("goalDifference");
+                                    data.add(TData);
+                                }
 
-                            for(int i=0;i<jArray.length();i++){
-                                JSONObject json_data = jArray.getJSONObject(i);
-                                Team TData = new Team();
-                                TData.name= json_data.getString("teamName");
-                                TData.points=json_data.getString("points");
-                                TData.played=json_data.getString("playedGames");
-                                TData.win=json_data.getString("wins");
-                                TData.draw=json_data.getString("draws");
-                                TData.lose=json_data.getString("losses");
-                                TData.gd=json_data.getString("goalDifference");
-                                data.add(TData);
+                                RecyclerView mTeamlist = (RecyclerView) view.findViewById(R.id.recycler_view);
+                                mAdapter = new AdapterTeam(getActivity(), data);
+
+
+                                mTeamlist.setLayoutManager(new LinearLayoutManager(getActivity()));
+                                mTeamlist.setAdapter(mAdapter);
+
+                                Toast.makeText(getActivity(), "MatchWeek:" + matchweek, Toast.LENGTH_SHORT).show();
+
+
+                            } catch (JSONException e) {
+                                Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
                             }
-
-                            RecyclerView mTeamlist = (RecyclerView)view.findViewById(R.id.recycler_view);
-                            mAdapter = new AdapterTeam(getActivity(), data);
-
-
-                            mTeamlist.setLayoutManager(new LinearLayoutManager(getActivity()));
-                            mTeamlist.setAdapter(mAdapter);
-
-
-
-
-                        } catch (JSONException e) {
-                            Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
                         }
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
 
-        return view;
+            return view;
+        }else{
+            View view = inflater.inflate(R.layout.no_internet, container, false);
+            return view;
+        }
     }
 }
