a609ac783ddb938b265cfbcbe4318aa5f72c6dc7
Darkmatter865 <darkmatter865@gmail.com>
2017-06-08 14:57:48 +0530

    About Dialog box added

--- app/src/main/java/com/example/vinay/tabactivity/MainActivity.java
+++ app/src/main/java/com/example/vinay/tabactivity/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.vinay.tabactivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     TabLayout tabLayout;
     Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar)findViewById(R.id.app_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
@@ -25,4 +39,39 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.About:
+                MaterialStyledDialog dialog = new MaterialStyledDialog.Builder(this)
+                        .setTitle("Liked this app ?")
+                        .withDialogAnimation(true)
+                        .setScrollable(true)
+                        .setIcon(R.mipmap.ic_launcher)
+                        .withIconAnimation(true)
+                        .setPositiveText("OK Cool, Let's GO !")
+                        .setNegativeText("Maybe Later")
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Darkmatter865")));
+                            }
+                        })
+                        .setDescription("Check me out on GitHub !")
+                        .build();
+                dialog.show();
+                // Do whatever you want to do on logout click.
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
